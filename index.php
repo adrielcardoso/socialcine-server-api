62d8b05616bc5441eac3db675eab7967dd969526
Adriel <adrielcardoso99@gmail.com>
2015-06-05 17:57:51 -0300

    make it better

--- index.php
+++ index.php
@@ -4,14 +4,13 @@ require_once './MainController.php';
 
 ini_set('display_errors', 'off');
 ini_set('session.gc_maxlifetime', 1260000);
-date_default_timezone_set('America/Sao_Paulo');
 header('Content-Type: application/json');
 header("HTTP/1.1 200 OK");
 header("Access-Control-Allow-Headers:origin, content-type, accept, api-key, api-secret, x-requested-with");
 header("Access-Control-Allow-Methods:POST, GET, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Credentials: true");
-date_default_timezone_set('UTC');
+date_default_timezone_set("Brazil/East"); // para aws 
 set_time_limit(0);
 
 new MainController();
